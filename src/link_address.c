a7479986cdfe5dffe534d4c5f728ca948d634c5d
Dan Molik <dan@d3fy.net>
2017-08-17 17:01:13 -0400

    Adding addressing to veth1

--- src/link_address.c
+++ src/link_address.c
@@ -110,12 +110,95 @@ int main(void)
 	msg.msg_iovlen = 1;
 
 	recvmsg(fd, &msg, 0);
-	close(fd);
+	// close(fd);
 	struct nlmsghdr *ret = (struct nlmsghdr*)buf;
 	if (ret->nlmsg_type == NLMSG_ERROR) {
 		struct nlmsgerr *err = (struct nlmsgerr*)NLMSG_DATA(ret);
 		if (err->error < 0) {
-			printf("error: %d\n, failed to create links\n", err->error);
+			printf("error: %d, failed to create links\n", err->error);
+			return 1;
+		}
+	} else {
+		printf("failed to create links\n");
+		return 1;
+	}
+
+	int addrlen = sizeof(struct in_addr);
+
+	memset(nlmsg, 0, 4096);
+	nlmsg->nlmsg_len   = sizeof(struct ifaddrmsg);
+	nlmsg->nlmsg_flags = NLM_F_ACK|NLM_F_REQUEST|NLM_F_CREATE|NLM_F_EXCL;
+	nlmsg->nlmsg_type  = RTM_NEWADDR;
+
+	struct ifaddrmsg *ifa = (struct ifaddrmsg *) NLMSG_DATA(nlmsg);
+	ifa->ifa_prefixlen = atoi("24");
+	if (!(ifa->ifa_index = if_nametoindex("veth1"))) {
+		printf("failed to get veth1 name: %s\n", strerror(errno));
+		return 1;
+	}
+	ifa->ifa_family = AF_INET;
+	ifa->ifa_scope = 0;
+
+	rtalen = RTA_LENGTH(addrlen);
+	rta->rta_len  = rtalen;
+
+	struct in_addr addr;
+	struct in_addr bcast;
+
+	if (inet_pton(AF_INET, "172.16.1.1", &addr) < 0) {
+		return -1;
+	}
+	if (inet_pton(AF_INET, "255.255.255.0", &bcast) < 0) {
+		return -1;
+	}
+
+	rta = NLMSG_TAIL(nlmsg);
+	rta->rta_type = IFA_LOCAL;
+	memcpy(RTA_DATA(rta), &addr, addrlen);
+	nlmsg->nlmsg_len = NLMSG_ALIGN(nlmsg->nlmsg_len) + RTA_ALIGN(rtalen);
+
+	rta = NLMSG_TAIL(nlmsg);
+	rta->rta_type = IFA_ADDRESS;
+	memcpy(RTA_DATA(rta), &addr, addrlen);
+	nlmsg->nlmsg_len = NLMSG_ALIGN(nlmsg->nlmsg_len) + RTA_ALIGN(rtalen);
+
+	rta = NLMSG_TAIL(nlmsg);
+	rta->rta_type = IFA_BROADCAST;
+	memcpy(RTA_DATA(rta), &bcast, addrlen);
+	nlmsg->nlmsg_len = NLMSG_ALIGN(nlmsg->nlmsg_len) + RTA_ALIGN(rtalen);
+
+	iov.iov_base = nlmsg;
+	iov.iov_len  = nlmsg->nlmsg_len;
+
+	memset(&msg, 0, sizeof(msg));
+	msg.msg_name = sa;
+	msg.msg_namelen = sizeof(*sa);
+	msg.msg_iov = &iov;
+	msg.msg_iovlen = 1;
+
+	if (sendmsg(fd, &msg, 0) < 0) {
+		fprintf(stderr, "failed to get socket: %s\n", strerror(errno));
+		exit(1);
+	}
+
+	len = 4096;
+	memset(buf, 0, len);
+	iov.iov_base = buf;
+	iov.iov_len = len;
+
+	memset(&msg, 0, sizeof(msg));
+	msg.msg_name = sa;
+	msg.msg_namelen = sizeof(*sa);
+	msg.msg_iov = &iov;
+	msg.msg_iovlen = 1;
+
+	recvmsg(fd, &msg, 0);
+	close(fd);
+	ret = (struct nlmsghdr*)buf;
+	if (ret->nlmsg_type == NLMSG_ERROR) {
+		struct nlmsgerr *err = (struct nlmsgerr*)NLMSG_DATA(ret);
+		if (err->error < 0) {
+			printf("error: %d, failed to add address to veth1\n", err->error);
 			return 1;
 		}
 	} else {
